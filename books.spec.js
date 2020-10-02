0a1dc98806d0709ca2f1bdbc6bfa8fe8417f2692
Lorr-Onyia <72157242+Lorr-Onyia@users.noreply.github.com>
2020-10-02 19:53:31 +0100

    Update books.spec.js

--- books.spec.js
+++ books.spec.js
@@ -1,11 +1,17 @@
-const library = require('./functions');
+const bookApp = require('./functions');
 
-
-test('available books should contain list of objects', () => {
-    let books = [{id: 11, name:"half of a yellow son" }];
-    expect(books).toEqual([{id: 11, name:"half of a yellow son" }]);
-});
-
-test('borrowed books should be an array', () => {
-    expect(removedBooks).toBe();
-});
+describe("bookApp", () => {
+    it('should exist', () => {
+        expect(bookApp).toBeDefined();
+    });
+    test('books should return an array of books', () => {
+        let books = [{id: 11, name:"half of a yellow sun"}]
+        expect(books).toEqual([{id: 11, name:"half of a yellow sun"}]);
+    });
+    test("onBorrowBook should be defined", () => {
+        expect(bookApp.onBorrowBook).toBeDefined();
+    });
+    test("onReturnBook should be defined", () => {
+        expect(bookApp.onReturnBook).toBeDefined();
+    });
+})
